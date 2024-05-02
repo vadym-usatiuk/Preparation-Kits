04207b769e53d3f7efd2baf00ada2ad0e6fa9f96
vadym-usatiuk <51781534+vadym-usatiuk@users.noreply.github.com>
2024-05-02 11:46:08 -0400

    Mock Test 10 - Pairs

--- Solution_089.java
+++ Solution_089.java
@@ -6,13 +6,7 @@ Given an array of integers and a target value, determine the number of pairs of 
 */
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
